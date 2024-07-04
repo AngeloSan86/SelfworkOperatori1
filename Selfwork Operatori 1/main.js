ec552dae3859abb6944033db0e04d6894bb9b795
Angelo Sanfilippo <124434790+AngeloSan86@users.noreply.github.com>
2024-07-04 15:40:53 +0200

    Add files via upload

---
+++ Selfwork Operatori 1/main.js
@@ -0,0 +1,10 @@
+let num1 = Math.floor(Math.random() * 101);
+let num2 = Math.floor(Math.random() * 101);
+let num3 = Math.floor(Math.random() * 101);
+let num4 = Math.floor(Math.random() * 101);
+let num5 = Math.floor(Math.random() * 101);
+
+let somma = num1 + num2 + num3 + num4 + num5;
+let media = somma / 5;
+
+console.log(`I 5 numeri generati casualmente tra 1 e 100 sono: ${num1}, ${num2}, ${num3}, ${num4}, ${num5}. La somma tra i numeri equivale a ${somma} e la media equivale a ${num1}.`);
